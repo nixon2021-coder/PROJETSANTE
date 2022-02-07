f6fdb3b43815d35ba42fa958d0d65ff8289eb73d
iMAC_10 <imac_10@iMAC-10.local>
2022-02-07 16:31:39 +0000

    modification projet

--- projetfinal.py
+++ projetfinal.py
@@ -1,6 +1,7 @@
 
 from cProfile import label
 from email import message
+import sqlite3
 from tkinter import*
 from tkinter import ttk
 from tkinter import font
@@ -68,7 +69,7 @@ def ajouter():
 
    
 
-def modifier ():
+def quitter ():
     Nom = entrernom.get()
     Prenom = entrerprenom.get()
     Age = entrerage.get()
@@ -96,53 +97,54 @@ enregistrement = Label(projet,bd = 10, relief = GROOVE, text = "VEUILLEZ ENREGIS
 enregistrement.place(x =150, y =80, width=1000)
 
 Nom = Label(projet, text= "NOM DU PATIENT", fg="green", bg="yellow", font=("italic", 16))
-Nom.place(x=350, y=150, width=200)
+Nom.place(x=0, y=200, width=200)
 entrernom = Entry(projet)
-entrernom.place(x=550, y=148, width=200)
+entrernom.place(x=200,y=197,width=300,height=30)
+
 
 Prenom = Label(projet, text= "PRENOM DU PATIENT", fg="green", bg="yellow", font=("italic", 16))
-Prenom.place(x=350, y=200, width=200)
+Prenom.place(x=0, y=250, width=200)
 entrerprenom = Entry(projet)
-entrerprenom.place(x=550, y=198, width=200)
+entrerprenom.place(x=200,y=247,width=300,height=30)
 
 age = Label(projet, text= "AGE DU PATIENT", fg="green", bg="yellow", font=("italic", 16))
-age.place(x=350, y=250, width=200)
+age.place(x=0, y=300, width=200)
 entrerage = Entry(projet)
-entrerage.place(x=550, y=248, width=200)
+entrerage.place(x=200,y=297,width=300,height=30)
 
 adresse = Label(projet, text= "ADRESSE DU PATIENT", fg="green", bg="yellow", font=("italic", 16))
-adresse.place(x=350, y=300, width=200)
+adresse.place(x=0, y=350, width=200)
 entreradresse = Entry(projet)
-entreradresse.place(x=550, y=298, width=200)
+entreradresse.place(x=200,y=347,width=300,height=30)
 
 
 Telephone = Label(projet, text= "TELEPHONE DU PATIENT", fg="green", bg="yellow", font=("italic", 16))
-Telephone.place(x=350, y=350, width=200)
+Telephone.place(x=0, y=400, width=200)
 entrerTelephone = Entry(projet)
-entrerTelephone.place(x=550, y=348, width=200)
+entrerTelephone.place(x=200,y=397,width=300,height=30)
 
 
 docteur = Label(projet, text = "NOM DU DOCTEUR", fg= "green", bg= "yellow", font =("italic", 16))
-docteur.place(x=350, y=400, width=200)
+docteur.place(x=0, y=450, width=200)
 combodocteur=["Mr YAO", "Mr KOFFI", "Mr YLY", "Mr BROU"]
 docliste = ttk.Combobox(projet, values=combodocteur)
 docliste.set('')
-docliste.place(x =550, y =399, width=200)
+docliste.place(x =200, y =449, width=200)
 
 
 chambre = Label(projet, text = "NUMERO DE CHAMBRE", fg= "green", bg= "yellow", font =("italic", 16))
-chambre.place(x=350, y=450, width=200)
+chambre.place(x=0, y=500, width=200)
 listechambre=["CHAMBRE 1", "CHAMBRE 2", "CHAMBRE 3", "CHAMBRE 4"]
 listeCombo = ttk.Combobox(projet, values=listechambre)
 listeCombo.set('')
-listeCombo.place(x =550, y =449, width=200)
+listeCombo.place(x =200, y =499, width=200)
 
 genre = Label(projet, text= "GENRE", font =("arial",16), bg ="yellow", fg="green")
-genre.place(x=350, y=499, width=200)
+genre.place(x=0, y=550, width=200)
 listegenre=["Masculin", "FEMININ"]
 genre = ttk.Combobox(projet, values=listegenre)
 genre.set('')
-genre.place(x =550, y =499, width=200)
+genre.place(x =200, y =549, width=200)
 
 
 #Radiobutton(projet, text="HOMME",padx= 5, value=1).place(x=650, y=500)
@@ -150,15 +152,41 @@ genre.place(x =550, y =499, width=200)
 #Radiobutton(projet, text="FEMME",padx= 5, value=2).place(x=550, y=500)
 
 btnenregistrer =Button(projet, text= "Enregistrer", font=("Arial", 16),bg = "green", fg = "red", command = ajouter)
-btnenregistrer.place(x=350,y=600,width=200,height=30)
+btnenregistrer.place(x=40,y=600,width=200,height=30)
 
-
-btnmodifier =Button(projet, text= "Modifier", font=("Arial", 16),bg = "green", fg = "red", command = modifier)
-btnmodifier.place(x=600,y=600,width=200,height=30)
+btnmodifier =Button(projet, text= "quitter", font=("Arial", 16),bg = "green", fg = "red", command = projet.destroy)
+btnmodifier.place(x=250,y=600,width=200,height=30)
 
 
 btnsupprimer =Button(projet, text= "Supprimer", font=("Arial", 16),bg = "green", fg = "red", command = supprimer)
-btnsupprimer.place(x=450,y=650,width=200,height=30)
+btnsupprimer.place(x=150,y=650,width=200,height=30)
+
+
+
+#-----LISTE DES PATIENTS---------
+patient = Label(projet, text = "LISTES DES PATIENTS", font = ("arial", 16), bg ="red", fg ="black")
+patient.place(x=600, y=140, width=650)
+
+tree = ttk.Treeview(projet, columns = (1,2,3,4,5,6,7,8), height=6, show = "headings")
+tree.place(x=600, y=200 , width=700, height=350)
+
+tree.heading(1 , text="Nom")
+tree.heading(2, text = "Prenom")
+tree.heading(3, text = "Age")
+tree.heading(4, text = "adresse")
+tree.heading(5, text = "Telephone")
+tree.heading(6, text = "Nom du Docteur")
+tree.heading(7, text = "Numero de chambre")
+tree.heading(8, text = "Genre")
+
+tree.column(1 , width=50)
+tree.column(2 , width=100)
+tree.column(3 , width=100)
+tree.column(4 , width=100)
+tree.column(5 , width=100)
+tree.column(6 , width=100)
+tree.column(7 , width=120)
+tree.column(8 , width=40)
 
 
 
